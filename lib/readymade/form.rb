bee000301d81dd5f62f19b69388893ebe8c61d44
OrestF <orest.f@coaxsoft.com>
2021-11-05 14:01:24 +0200

    Update version

--- lib/readymade/form.rb
+++ lib/readymade/form.rb
@@ -84,8 +84,8 @@ module Readymade
     def sync_errors(from: self, to: record)
       return if [from, to].any?(&:blank?)
 
-      errors = from.errors.instance_variable_get('@messages')
-      errors.to_h.merge!(to.errors.instance_variable_get('@messages'))
+      errors = from.errors.instance_variable_get('@messages').to_h
+      errors.merge!(to.errors.instance_variable_get('@messages').to_h)
 
       to.errors.instance_variable_set('@messages', errors)
       to.errors.messages.transform_values!(&:uniq) # Does not work with rails 6.1
